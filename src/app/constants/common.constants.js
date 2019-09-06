c53738901680f4c64b1b15cd8a9fadd748440c2a
Manoj kumar <ankammanoj@gmail.com>
2019-09-06 20:11:05 +0530

    COmmented by manoj at 811PM

--- src/app/constants/common.constants.js
+++ src/app/constants/common.constants.js
@@ -1,4 +1,4 @@
-export const CommonConstants={
-    API_URL:"https://practiceapi.devmountain.com/api/tasks"
-    API:"Help url by manoj"
-}
+export const CommonConstants = {
+  API_URL: "https://practiceapi.devmountain.com/api/tasks"
+  // API:"Help url by manoj" commented by manoj at 810PM
+};
